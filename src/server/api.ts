550e3dcd61d3e12a9da8c27c2e48af2f61387c64
Denis Donici <skryonline@gmail.com>
2025-08-15 12:43:33 +0300

    Simplify DB connection

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,34 +1,9 @@
 import { remultApi } from 'remult/remult-sveltekit';
-import { PostgresDataProvider } from "remult/postgres"
-import { Pool } from 'pg';
+import { createPostgresDataProvider } from "remult/postgres"
 import { env } from '$env/dynamic/private';
-import { SqlDatabase } from 'remult';
 import { Country } from '../shared/Countries';
 
-console.log({ user: env.DB_USER });
-
-
-console.log('MADE A NEW POOL >>>>>');
-const pg = new Pool({
-    host: env.DB_HOST,
-    port: parseInt(env.DB_PORT) || 5432,
-    user: env.DB_USER,
-    password: env.DB_PASSWORD,
-    database: env.DB_NAME
-});
-console.log(pg);
-
-console.log('MADE A NEW PG DATA PROVIDER >>>>>');
-const sqlDatabase = new PostgresDataProvider(pg);
-console.log(sqlDatabase);
-
-console.log('MADE A NEW SQL DATABASE >>>>>');
-const dataProvider = new SqlDatabase(sqlDatabase);
-console.log(dataProvider);
-
-
-console.log('EXPORTING REMULT API >>>>>');
 export const api = remultApi({
-    dataProvider,
+    dataProvider: createPostgresDataProvider({ connectionString: env.DATABASE_URL }),
     entities: [Country]
 });
