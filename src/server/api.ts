a75ab9f3d9962fa926d0e29270831d8ca970ac87
Denis Donici <skryonline@gmail.com>
2025-08-15 12:29:35 +0300

    Logging DB providing management

--- src/server/api.ts
+++ src/server/api.ts
@@ -7,16 +7,28 @@ import { Country } from '../shared/Countries';
 
 console.log({ user: env.DB_USER });
 
+
+console.log('MADE A NEW POOL >>>>>');
 const pg = new Pool({
     host: env.DB_HOST,
     port: parseInt(env.DB_PORT) || 5432,
     user: env.DB_USER,
     password: env.DB_PASSWORD,
     database: env.DB_NAME
+});
+console.log(pg);
 
-})
+console.log('MADE A NEW PG DATA PROVIDER >>>>>');
+const sqlDatabase = new PostgresDataProvider(pg);
+console.log(sqlDatabase);
 
+console.log('MADE A NEW SQL DATABASE >>>>>');
+const dataProvider = new SqlDatabase(sqlDatabase);
+console.log(dataProvider);
+
+
+console.log('EXPORTING REMULT API >>>>>');
 export const api = remultApi({
-    dataProvider: new SqlDatabase(new PostgresDataProvider(pg)),
+    dataProvider,
     entities: [Country]
 });
